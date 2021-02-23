98a69f33dcfefb63f95669b48976c600aeca14a1
Fernando Gaffo <fernandogaffo@gmail.com>
2021-02-23 19:58:28 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -36,7 +40,25 @@ public class SellerFormController implements Initializable {
 	private TextField txtNome;
 
 	@FXML
+	private TextField txtEmail;
+
+	@FXML
+	private DatePicker dpDataNasc;
+
+	@FXML
+	private TextField txtSalario;
+
+	@FXML
 	private Label lblErrorNome;
+
+	@FXML
+	private Label lblErrorEmail;
+
+	@FXML
+	private Label lblErrorDataNasc;
+
+	@FXML
+	private Label lblErrorSalario;
 
 	@FXML
 	private Button btnSalvar;
@@ -83,7 +105,10 @@ public class SellerFormController implements Initializable {
 
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtNome, 30);
+		Constraints.setTextFieldMaxLength(txtNome, 150);
+		Constraints.setTextFieldDouble(txtSalario);
+		Constraints.setTextFieldMaxLength(txtEmail, 150);
+		Utils.formatDatePicker(dpDataNasc, "dd/MM/yyyy");
 	}
 
 	public void setSeller(Seller entity) {
@@ -96,6 +121,12 @@ public class SellerFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtNome.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtSalario.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpDataNasc.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
 
 	public void setSellerService(SellerService service) {

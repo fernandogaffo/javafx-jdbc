4ec4cba124f5eb49b885bf48de2b5a292d9962b7
Fernando Gaffo <fernandogaffo@gmail.com>
2021-02-23 19:36:42 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,135 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	private Seller entity;
+	private SellerService service;
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+	@FXML
+	private TextField txtId;
+
+	@FXML
+	private TextField txtNome;
+
+	@FXML
+	private Label lblErrorNome;
+
+	@FXML
+	private Button btnSalvar;
+
+	@FXML
+	private Button btnCancelar;
+
+	@FXML
+	public void onBtnSalvarAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity is null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service is null");
+		}
+
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		} catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+	}
+
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
+	@FXML
+	public void onBtnCancelarAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtNome, 30);
+	}
+
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity is null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtNome.setText(entity.getName());
+	}
+
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+
+		ValidationException exception = new ValidationException("Validation Error");
+
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtNome.getText() == null || txtNome.getText().trim().equals("")) {
+			exception.addError("nome", "Informação obrigatória.");
+		}
+		obj.setName(txtNome.getText());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+
+		return obj;
+	}
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("nome")) {
+			lblErrorNome.setText(errors.get("nome"));
+		}
+	}
+}
